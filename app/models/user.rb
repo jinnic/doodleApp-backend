768e8ea3c7e2ba69e63da962bf4a4787e6810056
aleksarad <alexa.radevich1@gmail.com>
2020-07-28 23:38:53 -0400

    added validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :user_name, uniqueness: { case_sensitive: true }
+  validates :user_name, presence: true
   has_many :doodles, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
